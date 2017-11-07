e534b54a085df551bc585443d46ffd1310641617
Whellcome <deims@narod.ru>
2017-11-07 23:24:58 +1000

    637 routes filters were done

--- modules/custom/natura_local_sform/js/nl-fsorm.js
+++ modules/custom/natura_local_sform/js/nl-fsorm.js
@@ -11,6 +11,30 @@
           return false;
         }
       });
+
+      $(document).on("click", '#views-exposed-form-nl-search-page-2 .enable-values', function () {
+        updateResults();
+      });
+      $(document).on("click", '.reset-values', function () {
+        $(context).find("#views-exposed-form-nl-search-page-2 input[type=checkbox]").each(function() {
+          $(this).removeAttr("checked");
+        });
+        updateResults();
+      });
+      $(context).find('details summary').once('added-span').append('<span class="checked-count"></span>');
+      $(context).find('span.checked-count').text(function () {
+        var detail = $(this).parents('details').eq(0);
+        //alert(detail.hasClass('form-item'));
+        var count = detail.find('input:checkbox:checked').length;
+        if(count){
+          return count;
+        }
+        return'';
+      });
     }
   };
+
+  function updateResults(){
+    $("#views-exposed-form-nl-search-page-2 .form-submit").trigger('click');
+  }
 })(jQuery);
